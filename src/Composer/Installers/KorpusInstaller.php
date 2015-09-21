88ae1f438c24b96e4da66086a85a6117da90d2ba
bluematt <bluematt@gmail.com>
2015-09-21 15:26:25 +0100

    Update KorpusInstaller.php

--- src/Composer/Installers/KorpusInstaller.php
+++ src/Composer/Installers/KorpusInstaller.php
@@ -3,9 +3,6 @@ namespace Composer\Installers;
 class KorpusInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'pages' => 'data2/pages/',
-        'templates' => 'data2/templates/',
-        'shared' => 'data2/shared/',
-        'cache' => 'data2/cache/',
+        'korpus-data' => 'data2/',
     );
 }
